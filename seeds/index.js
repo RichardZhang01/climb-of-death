7a513828eda51d2c9c129851518d0bcc91151e6b
Trent_D <tdickson91@gmail.com>
2022-06-29 13:23:13 -0400

    require scenario and choice seeds, await sequelize, seedScenarios, and seedChoices, also console logs to ensure function is running

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,14 @@
 const sequelize = require('../config/connection');
+const seedScenarios = require('./scenario-seeds');
+const seedChoices = require('./choices-seeds');
 
 const seedAll = async () => {
-
+    await sequelize.sync({ force: true });
+    console.log("\nDatabase Sync!\n");
+    await seedScenarios();
+    console.log("\nScenarios seeded!\n");
+    await seedChoices();
+    console.log("\nChoices seeded!\n");
     process.exit(0);
 };
 
