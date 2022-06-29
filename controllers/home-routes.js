40a39d7e0124cc5e4fcba17695959d8fee90b8df
RZhang <richardzhiyuanzhang@gmail.com>
2022-06-28 22:07:04 -0400

    created basic routes for displaying homepage, login, and signup pages. Also implemented routes for signing up and logging which direct to the gamepage. homeroutes also all redirect to gamepage if user is logged in. Copied over a basic login.js and signup.js form handler for the login and signup pages respectively. Routes and logic will likely need to be tweaked as front-end is implemented.

---
+++ controllers/home-routes.js
@@ -0,0 +1,28 @@
+const homepage = require('express').Router();
+// const { User } = require('../models');
+
+homepage.get('/', (req, res) => {       
+    if (req.session.loggedIn) {
+        res.redirect('/gamepage');
+        return;
+    }
+    res.render('homepage');
+});
+
+homepage.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/gamepage');
+        return;
+    }
+    res.render('login');
+});
+
+homepage.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/gamepage');
+        return;
+    }
+    res.render('signup');
+});
+
+module.exports = homepage;
